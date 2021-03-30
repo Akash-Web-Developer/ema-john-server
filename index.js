be7953b52531f8c27e057e8d38af8c0152db5b06
Abdullah-Al-Akash <aabdullahalakash@.com>
2021-03-30 20:33:27 +0600

    My server Code

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
+
+require('dotenv').config()
+
+//for Connection DB Purpose
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7gqmj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+port = 5000;
+
+//For Connection with DataBase
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("emaJohnStore").collection("products");
+  const ordersCollection = client.db("emaJohnStore").collection("Orders");
+
+  app.post("/addProduct", (req, res) => {
+    const products = req.body;
+    productsCollection.insertOne(products)
+    .then(result => {
+      console.log(result.insertCount)
+      res.send(result.insertCount);
+    })
+  })
+
+  app.get('/products', (req, res) => {
+    productsCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+
+  app.get('/products/:key', (req, res) => {
+    productsCollection.find({key: req.params.key})
+    .toArray((err, documents) => {
+      res.send(documents[0]);
+    })
+  })
+
+  app.post('/productsByKeys', (req, res) => {
+    const productKeys = req.body;
+    productsCollection.find({key: {$in: productKeys}})
+    .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
+
+
+  app.post("/addOrder", (req, res) => {
+    const order = req.body;
+    ordersCollection.insertOne(order)
+    .then(result => {
+      res.send(result.insertCount > 0);
+    })
+  })
+  
+});
+
+
+app.listen(port);
